acc33fbbb45bd1e2fc7d62fca57d70589cbde619
Alexander Dyrikov <dyrikov.a@gmail.com>
2021-11-05 17:42:16 +0300

    refactor code

--- src/test/java/service/TestDataReader.java
+++ src/test/java/service/TestDataReader.java
@@ -4,7 +4,7 @@ import java.util.ResourceBundle;
 
 public class TestDataReader
 {
-    private static ResourceBundle resourceBundle = ResourceBundle.getBundle("dev");
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(System.getProperty("environment"));
 
     public static String getTestData(String key)
     {
